998a1a5a24156004445dbe141acaa3b33c08c0a9
danestia <dan.fleming@estia.fr>
2026-04-29 11:37:11 +0200

    adding configuration manager for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,39 @@ from fastapi.testclient import TestClient
 
 from app.db.base import Base
 from app.main import app
-from app.db.session import SessionLocal
 
 @pytest.fixture(scope="function")
 def test_engine():
     engine = create_engine(
-        "sqlite:///:memory:"
+        "sqlite:///:memory:",
         connect_args={"check_same_thread": False}
     )
     Base.metadata.create_all(bind=engine)
     return engine
+
+@pytest.fixture(scope="function")
+def db_session(test_engine):
+    TestingSessionLocal = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=test_engine
+    )
+    session = TestingSessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
+@pytest.fixture(scope="function")
+def client(db_session, monkeypatch):
+
+    def override_get_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    from app.ingestion import get_db
+    app.dependency_overrides[get_db] = override_get_db
+
+    return TestClient(app)
