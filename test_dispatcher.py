b2652e4a55729a848043a17e7ad6f93797c15416
danestia <dan.fleming@estia.fr>
2026-07-23 10:52:11 +0200

    Adding MQTT broker

--- test_dispatcher.py
+++ test_dispatcher.py
@@ -6,7 +6,7 @@ def run_test():
         'power_percentage': [51.4, 71.7, 88.2, 96.2, 98.4, 94.8, 85.4, 70.3, 51.0, 29.4]
     }, index = pd.date_range('2026-07-21 08:00', period=10, freq='h'))
 
-    pi_ip = <PI IP ADDRESS HERE>
+    pi_ip = '172.22.111.96'
 
     dispatcher = PlantformMQTTDispatcher(broker_ip=pi_ip)
 
